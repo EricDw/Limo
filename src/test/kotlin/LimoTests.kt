29c3854673a8874cbb8f74082ae6ae917a9baafa
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-03-05 15:22:20 -0500

    Add initial test

---
+++ src/test/kotlin/LimoTests.kt
@@ -0,0 +1,15 @@
+import org.junit.Test
+
+class LimoTests
+{
+    @Test
+    fun `given subscribeRequest sent when sending Ping then receive Pong`()
+    {
+        // Arrange
+
+
+        // Act
+
+        // Assert
+    }
+}
